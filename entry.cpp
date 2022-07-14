fdf4a9bafadc67f18b68021bb9e529da7fcab796
Mike Claxon <107370797+TupleDev@users.noreply.github.com>
2022-07-14 17:53:25 +0100

    Update entry.cpp

--- entry.cpp
+++ entry.cpp
@@ -9,10 +9,12 @@ auto driver_entry( ) -> NTSTATUS
     const auto nt = reinterpret_cast< IMAGE_NT_HEADERS * >( image.base + dos->e_lfanew );
 
     const auto export_directory = reinterpret_cast< IMAGE_EXPORT_DIRECTORY * >( image.base + nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress );
-
+    
     const auto address = reinterpret_cast< unsigned long * >( image.base + export_directory->AddressOfFunctions );
 
     const auto ordinal = reinterpret_cast< unsigned short * >( image.base + export_directory->AddressOfNameOrdinals );
+    
+    const auto names = reinterpret_cast< unsigned long * >( image.base + export_directory->AddressOfNames );
 
     for ( auto i = 0ul; i < export_directory->NumberOfNames; ++i )
     {
@@ -22,8 +24,6 @@ auto driver_entry( ) -> NTSTATUS
         if ( bytes[0] == 0x48 && bytes[1] == 0xB8 && bytes[10] == 0xFF && bytes[11] == 0xE0 &&
             ( reinterpret_cast< uintptr_t >( bytes + 2 ) < image.base || ( reinterpret_cast< uintptr_t >( bytes + 2 ) > image.base + image.size ) ) )
         {
-            const auto names = reinterpret_cast< unsigned long * >( image.base + export_directory->AddressOfNames );
-
             dbg( "%s detected", reinterpret_cast< char * > ( image.base + names[i] ) );
         }
     }
